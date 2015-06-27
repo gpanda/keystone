03e23e03d8d3a3b97c0e4ecd372e7431f82cdf1a
Le Tian Ren <renlet@cn.ibm.com>
2015-06-27 15:02:36 +0800

    Remove those not expected to track.

--- gpanda/tmp/adminrc.sh
+++
@@ -1,4 +0,0 @@
-export OS_IDENTITY_API_VERSION=3
-# export OS_AUTH_URL=http://localhost:5000/v3
-# export OS_SERVICE_TOKEN=63ec6d1412beeae5b62b
-# export OS_SERVICE_ENDPOINT=http://localhost:35357/v3
